bd9d968a0e31f21976662adef6aeb40639a18561
MuhammadAdnan11 <adnan191022@gmail.com>
2023-12-11 14:23:40 +0500

    updated

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const app= express();
+
+app.get("/", (req,res)=>{
+     res.status(200).send("Welcome to Adnan Khan");
+});
+
+app.get("/register", (req,res)=>{
+    res.status(200).send("Welcome to registration page");
+});
+
+
+//starting server
+const PORT=5000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running at port: ${PORT}`);
+});
