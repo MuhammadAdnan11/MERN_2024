e505e769da01adb59b8e1c3a06d0b9c5298c87d7
MuhammadAdnan11 <adnan191022@gmail.com>
2023-12-19 11:03:45 +0500

    env file added

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,9 +1,7 @@
 const mongoose= require('mongoose');
 
 
-// const URI= "mongodb+srv://Adnan:<password>@cluster0.8ia5dxa.mongodb.net/";
-// const URI= 'mongodb://localhost:27017/mern_admin';
-const URI='mongodb+srv://adnan191022:Adnan7527@mern.kxzwajf.mongodb.net/'
+const URI=process.env.MONGODB_URI;
 
 // mongoose.connect(URI)
 
@@ -11,7 +9,7 @@ const URI='mongodb+srv://adnan191022:Adnan7527@mern.kxzwajf.mongodb.net/'
 const connectDb= async()=>{
     try {
         await mongoose.connect(URI);
-        console.log('connection successful to database');
+        console.log('connection successful to database✅');
     } catch (error) {
         console.error("database connection failed");
         process.exit(0);
